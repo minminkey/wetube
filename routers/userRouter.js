14cba2c40a314d130fd77c04f3cb7f0f9a25f9ee
alsrl8259 <alsrl8259@gmail.com>
2021-02-03 18:06:52 +0900

    Router Setting

---
+++ routers/userRouter.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { changePassword, editProfile, userDetail, users } from "../controllers/userController";
+import routes from "../routes";
+
+const userRouter = express.Router();
+
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+
+export default userRouter;
---
+++ routers/videoRouter.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { deleteVideo, editVideo, upload, videoDetail, videos } from "../controllers/videoController";
+import routes from "../routes";
+
+const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
+
+export default videoRouter;
