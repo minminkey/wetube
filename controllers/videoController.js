c3419f2625b0cbfc75bfcefc038dc34addead9ce
alsrl8259 <alsrl8259@gmail.com>
2021-02-07 23:23:33 +0900

    Home Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,9 @@
-export const home = (req, res) => res.render("home", {pageTitle: "Home"});
+import {videos} from "../db"
+
+export const home = (req, res) => {
+
+  res.render("home", {pageTitle: "Home", videos});
+};
 
 export const search = (req, res) => {
     const {
