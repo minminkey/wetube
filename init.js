d88365c9f44b37bc1b16f69e2a6545184d32ec67
alsrl8259 <alsrl8259@gmail.com>
2021-02-11 22:22:20 +0900

    Configuring Dot Env

--- init.js
+++ init.js
@@ -1,8 +1,10 @@
 import "./db";
 import app from "./app";
+import app from "dotenv";
 
+dotenv.config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
