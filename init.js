5b268b0544463d75193781f580b303dca402023b
alsrl8259 <alsrl8259@gmail.com>
2021-02-13 00:45:53 +0900

    Comment Model

--- init.js
+++ init.js
@@ -2,7 +2,8 @@ import "./db";
 import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
-import "./models/video";
+import "./models/Video";
+import "./models/Comment";
 
 const PORT = process.env.PORT || 4000;
 
