8869373e8b1b6e09167b37edf5e891036b929f67
alsrl8259 <alsrl8259@gmail.com>
2021-03-03 16:50:09 +0900

    Edit User Profile

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,14 +2,17 @@ import express from "express";
 import {
   changePassword,
   getEditProfile,
+  postEditProfile,
   userDetail,
 } from "../controllers/userController";
-import { onlyPrivate } from "../middlewares";
+import { onlyPrivate, uploadAvatar } from "../middlewares";
 import routes from "../routes";
 
 const userRouter = express.Router();
 
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
+
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
