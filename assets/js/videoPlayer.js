ab691aec359de7854055e3dfcd113acb5010e0ac
alsrl8259 <alsrl8259@gmail.com>
2021-03-05 13:38:16 +0900

    Play Pause Functionallity

---
+++ assets/js/videoPlayer.js
@@ -0,0 +1,21 @@
+const videoContainer = document.getElementById("jsVideoPlayer");
+const videoPlayer = document.querySelector("#jsVideoPlayer video");
+const playBtn = document.getElementById("jsPlayButton");
+
+function handlePlayClick() {
+  if (videoPlayer.paused) {
+    videoPlayer.play();
+  } else {
+    videoPlayer.pause();
+  }
+}
+
+function playVideo() {}
+
+function init() {
+  playBtn.addEventListener("click", handlePlayClick);
+}
+
+if (videoContainer) {
+  init();
+}
