83453c979b13977c7d4645bd7a97eccbcb101ecb
alsrl8259 <alsrl8259@gmail.com>
2021-03-11 21:18:43 +0900

    MongoDB Atlas Connecting

--- db.js
+++ db.js
@@ -5,7 +5,8 @@ dotenv.config();
 
 mongoose.connect(
   // "mongodb://localhost:27017/we-tube",
-  process.env.MONGO_URL,
+  // process.env.MONGO_URL,
+  process.env.MONGO_URL_PROD,
   {
     useNewUrlParser: true,
     useFindAndModify: false,
